be2d68ba625b26a6e290f5455a53ffadffc926ec
Zubayer204 <67687459+Zubayer204@users.noreply.github.com>
2022-01-28 18:27:55 +0600

    Update settings.py

--- vdchat/settings.py
+++ vdchat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-toy3^l9bwz8muu0-mhnx%(tjbc%(-ntf4%y^i3qo-)t38ci@$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['zgroupchat.herokuapp.com', '127.0.0.1', 'localhost']
 
