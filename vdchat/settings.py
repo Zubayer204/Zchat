640e884d771589f0e3491fd4e5abb888ef72d861
Zubayer Rahaman <67687459+Zubayer204@users.noreply.github.com>
2022-12-19 13:26:28 +0600

    Update settings.py

--- vdchat/settings.py
+++ vdchat/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
